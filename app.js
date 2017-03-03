663a7bfb3ac6f6fccca303b2b3c40c3476cddd62
Ramon Reyes <reyesjunk@gmail.com>
2017-03-03 15:14:21 -0500

    add pseudo-code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //APPLICATION STATE
 //1- create state object
 //2- create state modfication functions
@@ -5,27 +6,58 @@
 //4- create event listeners
 
 
+
+
 //1- CREATE STATE OBJECT
+// Add apples, oranges, milk, and bread as default values
 var state = {
   items: [
 
   ]
-};
-
-//2- CREATE modfication functions
-
-
-function addItem(state, item){
-  state.items.push(item);
 }
+/*2- CREATE modfication functions  CRUD
+
+function addItem(userinput){
+	
+	add item. set default check state to false.
+}
+
+function checkItem() {
+	add checked state. true / false.
+}
+function removeItem(){
+	remove item from state
+}
+*/
+function addItem(state, item){
+
+}
+/*
+//3- CREATE RENDER FUNCTIONS
+founction renderDOM(){
+	render current state of list to the DOM
+}
+	function renderDOM(){}
+	function addfromDOM{}
+	function deletefromDOM(){}
+	function checkiteminDOM(){}
+
+*/
 
 function removeItem(state, itemID){
-  state.items.splice(itemID-1, 1);
+ 
 }
 
-addItem(state, 'a item');
-addItem(state, 'b item');
-addItem(state, 'c item');
-addItem(state, 'd item');
-console.log(state);
-removeItem(state, )
+
+
+//4- HANDLE EVENTS
+/*
+	load the List
+	listen for add item Click
+	listen for delete item Click
+    toggle item checked 
+
+*/
+$(document).ready(function(){
+
+});
